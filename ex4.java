f18fc6098246211d01e5cc92a92611c4b4d97f82
salim670 <yaseensalim003@gmail.com>
2026-05-25 20:47:03 -0700

    first commit

--- ex4.java
+++ ex4.java
@@ -7,7 +7,7 @@ public class ex4 {
 
    
     System.out.println("Enter a number:");
-    Scanner sc=new Scanner(System.in);
+     Scanner sc=new Scanner(System.in);
     int a=sc.nextInt();
     for(i=0;i<n;i++)
     {
