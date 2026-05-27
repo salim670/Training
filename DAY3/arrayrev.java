89e6634c0d9bb82c8153469aea45707729315bc8
salim670 <yaseensalim003@gmail.com>
2026-05-27 03:24:29 -0700

    day3

---
+++ DAY3/arrayrev.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class arrayrev {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt(); 
+}
+        for (int i = arr.length-1; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+}
